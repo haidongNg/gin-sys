de2b789baad4b8edae6b39d37a25ce9203a17bac
nguyend <nguyenhaidong@1412.com>
2024-07-15 14:18:00 +0700

    first commit

---
+++ src/main/java/com/sys/gin/GinSysApplication.java
@@ -0,0 +1,13 @@
+package com.sys.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sys/gin/GinSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.gin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
